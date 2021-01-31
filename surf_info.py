8c9a93d2f2c360b55e76a1c496abc5acefb1f9a3
ereid7 <evan@reids.us>
2021-01-30 17:58:31 -0800

    Add search functionality

--- surf_info.py
+++ surf_info.py
@@ -5,49 +5,69 @@ from rich.panel import Panel
 from rich.text import Text
 from rich.padding import Padding
 from rich.emoji import Emoji
-
+import sys
 import requests
 
-console = Console()
 
-# TODO take beach names as arguments and search surfline - get first link
+if __name__ == "__main__":
+  console = Console()
 
-with console.status("", spinner='shark') as status:
-  surf_url = 'https://www.surfline.com/surf-report/la-jolla-shores/5842041f4e65fad6a77088cc'
-  html_text = requests.get(surf_url).text
-  soup = BeautifulSoup(html_text, 'html.parser')
+  if (len(sys.argv) < 2):
+      console.print(f"Search argument required to find surf spot", style="cyan")
+      sys.exit()
+      
+  with console.status("", spinner='shark') as status:
 
-  title_container = soup.find('h1', { 'class' : 'sl-forecast-header__main__title'})
+    search = sys.argv[1]
 
-  surf_height_container = soup.find('div', { 'class' : 'quiver-spot-forecast-summary__stat-container--surf-height' })
-  height_container = surf_height_container.findChildren('span', { 'class' : 'quiver-surf-height'})[0]
-  height_description = surf_height_container.findChildren('span', { 'class' : 'quiver-reading-description'})[0]
+    # search for surf spot name
+    search_url = f"https://www.surfline.com/search/{search}"
+    search_page_html = requests.get(search_url).text
+    search_soup = BeautifulSoup(search_page_html, 'html.parser')
 
-  tide_container = soup.find('div', { 'class' : 'quiver-spot-forecast-summary__stat-container--tide'})
-  tide_height_container = tide_container.findChildren('span', { 'class' : 'quiver-reading'})[0]
-  tide_description_container = tide_container.findChildren('span', { 'class' : 'quiver-reading-description'})[0]
+    # retrieve first search result
+    first_result_container = search_soup.find('div', { 'class' : 'result'})
 
-  water_temp_container = soup.find('div', { 'class' : 'quiver-water-temp'}).findChildren('div')[0]
-  air_temp_container = soup.find('div', { 'class' : 'quiver-weather-stats'}).findChildren('div')[0]
-
-  title = Text(title_container.getText().strip())
-  title.stylize("bold magenta")
-  title_padding = Padding(title, (1, 0))
-
-  table = Table.grid(expand=True)
-  table.add_column(style='bold cyan', no_wrap=True)
-  table.add_column(style='magenta')
-  table.add_row('Surf Height', height_container.getText().strip())
-  table.add_row('', height_description.getText(separator=' ').strip())
-  table.add_row()
-  table.add_row('Tide Height', tide_height_container.getText().strip())
-  table.add_row('', tide_description_container.getText(separator=' ').strip())
-  table.add_row()
-  table.add_row('Water Temp', water_temp_container.getText().strip())
-  table.add_row()
-  table.add_row('Air Temp', air_temp_container.getText().strip())
+    if (first_result_container is None):
+      console.print(f"No surf spots found for: {search}", style="cyan")
+      sys.exit()
+  
+    first_result_link = first_result_container.findChildren('a')[0]
 
 
-console = Console()
-console.print(title_padding)
-console.print(table)
+    surf_url = first_result_link['href']
+    html_text = requests.get(surf_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+
+    title_container = soup.find('h1', { 'class' : 'sl-forecast-header__main__title'})
+
+    surf_height_container = soup.find('div', { 'class' : 'quiver-spot-forecast-summary__stat-container--surf-height' })
+    height_container = surf_height_container.findChildren('span', { 'class' : 'quiver-surf-height'})[0]
+    height_description = surf_height_container.findChildren('span', { 'class' : 'quiver-reading-description'})[0]
+
+    tide_container = soup.find('div', { 'class' : 'quiver-spot-forecast-summary__stat-container--tide'})
+    tide_height_container = tide_container.findChildren('span', { 'class' : 'quiver-reading'})[0]
+    tide_description_container = tide_container.findChildren('span', { 'class' : 'quiver-reading-description'})[0]
+
+    water_temp_container = soup.find('div', { 'class' : 'quiver-water-temp'}).findChildren('div')[0]
+    air_temp_container = soup.find('div', { 'class' : 'quiver-weather-stats'}).findChildren('div')[0]
+
+    title = Text(title_container.getText().strip())
+    title.stylize("bold magenta")
+    title_padding = Padding(title, (1, 0))
+
+    table = Table.grid(expand=True)
+    table.add_column(style='bold cyan', no_wrap=True)
+    table.add_column(style='magenta')
+    table.add_row('Surf Height', height_container.getText().strip())
+    table.add_row('', height_description.getText(separator=' ').strip())
+    table.add_row()
+    table.add_row('Tide Height', tide_height_container.getText().strip())
+    table.add_row('', tide_description_container.getText(separator=' ').strip())
+    table.add_row()
+    table.add_row('Water Temp', water_temp_container.getText().strip())
+    table.add_row()
+    table.add_row('Air Temp', air_temp_container.getText().strip())
+
+  console.print(title_padding)
+  console.print(table)
