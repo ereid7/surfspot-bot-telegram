a05150f219c2069e6e930278262b51dffe131148
ereid7 <evan@reids.us>
2021-02-08 19:45:48 -0800

    Update readme

--- src/main.py
+++ src/main.py
@@ -6,9 +6,10 @@ from handlers import surf
 import requests
 import re
 
+TELEGRAM_TOKEN='TOKEN'
 
 def main():
-  updater = Updater(token='TOKEN')
+  updater = Updater(token=TELEGRAM_TOKEN)
   dp = updater.dispatcher
   logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
